9abf55665515bf11c796944efd7b5c26f3a98ce4
Abhishek Kandi <kandii.abhishek@gmail.com>
2022-02-08 22:18:13 +0530

    Added product, response DTOs & Product repository interface

---
+++ JackRestaurant/Services/Jack.Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Jack.Services.ProductAPI.Models.Dto;
+
+namespace Jack.Services.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+
+        Task<ProductDto> GetProductById(int productId);
+
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+
+        Task<bool> DeleteProduct(int productId);
+    }
+}
